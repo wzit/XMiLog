2d3c7b92f49f081f54c7ec09d245fcb275b0eaf6
Xavier Michelon <xmichelo@gmail.com>
2008-03-19 19:24:14 +0000

    XMiLog: fixed iterator based for loops

--- XMiLog.cpp
+++ XMiLog.cpp
@@ -139,8 +139,7 @@ string Log::getTextLog(bool showType, bool showTime, const char *title) const
    string s;
    if (title)
    s += string(title) +"\n";
-   for (vector<LogEntry>::const_iterator iter = entries_.begin();iter < entries_.end(); 
-        iter++)
+   for (vector<LogEntry>::const_iterator iter = entries_.begin();iter != entries_.end(); iter++)
       s += (*iter).getText(showType, showTime) + "\n";
    return s;
 }
@@ -178,7 +177,7 @@ string Log::_getHTMLLog(string const& styleSheet,bool showType, bool showTime,
    if (title)
       s += string("<div class=\"title\">") + title + "</div>\n";
    s += "    <table class=\"log\">\n";
-   for (vector<LogEntry>::const_iterator iter = entries_.begin(); iter < entries_.end(); iter++)
+   for (vector<LogEntry>::const_iterator iter = entries_.begin(); iter != entries_.end(); iter++)
       s += (*iter).getHTML(showType, showTime) + "\n";
    return  s + "    </table>"
              + "<div class=\"footer\">Generated on " + now.getString() + "</div>\n"

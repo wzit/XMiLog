f5b4e0b94fb929f9b5bbccdb91bba7ecddf4c3e4
Xavier Michelon <xmichelo@gmail.com>
2011-06-03 15:00:25 +0200

    Fixed warning about non-literal strings passed to printf()

--- _test/XMiLogTest/main.cpp
+++ _test/XMiLogTest/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
       log.warn("this is a warn");
       log.fatal("this is a fatal");
    }
-   printf(log.getTextLog(true, true, logTitle.c_str()).c_str());
+   printf("%s", log.getTextLog(true, true, logTitle.c_str()).c_str());
    string htmlLog = log.getHTMLLog(true, true, logTitle.c_str());
    FILE *fp;
 #ifdef MACOS
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
       fprintf(stderr, "Error: could not open log file %s for writing\n", htmlFile.c_str()); 
       return 1;
    }
-   fprintf(fp, htmlLog.c_str());
+   fprintf(fp, "%s", htmlLog.c_str());
    fclose(fp);
    printf("A HTML version of this log has been saved to 'log.html'\n");
    return 0;
